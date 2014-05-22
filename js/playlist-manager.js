973ae4b58c16825d7992f6cb3ddffe4983b0d702
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2014-05-22 09:16:46 -0400

    Started working on playlist object

---
+++ js/playlist-manager.js
@@ -0,0 +1,14 @@
+/**
+ * Created by daniel.wilhelmsen on 5/18/14.
+ */
+var playlistManager = {
+    defaultPlaylist: [],
+    requests: [],
+    hasRequests: function(){
+        return this.requests.length > 0;
+    },
+    getPlaylist: function() {
+
+    }
+
+}
